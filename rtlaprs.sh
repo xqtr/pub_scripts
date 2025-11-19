1795d547dfad40714de8d6446c96e660d9141d70
xqtr <xqtr.xqtr@gmail.com>
2025-11-19 18:35:45 +0200

    update

--- rtlaprs.sh
+++ rtlaprs.sh
@@ -1,1 +1,1 @@
-rtl_fm -f 144.8M -g 30 -s 22050 -l 20 - | multimon -t raw -a AFSK1200 /dev/stdin
+rtl_fm -f 144.8M -g 30 -s 22050 -l 20 - | multimon-ng -a AFSK1200 -A -t raw - | sed -u -r 's/^.{6}//'
